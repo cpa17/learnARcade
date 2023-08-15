f2db5844b140e6a354172d0e5c02ee10477b39b8
cpa_17 <pascal.ahlner@gmx.de>
2023-08-15 14:58:49 +0200

    rolling dice

--- learnARcade/learnARcade/DiceView.swift
+++ learnARcade/learnARcade/DiceView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 import RealityKit
 
 struct DiceView : View {
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        
+        ZStack {
+            ARViewContainer().edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
@@ -19,45 +23,15 @@ private extension DiceView {
     
     struct ARViewContainer: UIViewRepresentable {
         
-        func makeUIView(context: Context) -> ARView {
-            
-            let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
-            let session = arView.session
-            let configuration = ARWorldTrackingConfiguration()
-            
-            configuration.planeDetection = .horizontal
-            session.run(configuration)
-            
-            let coachingOverlay = ARCoachingOverlayView()
-            coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            coachingOverlay.session = session
-            coachingOverlay.goal = .horizontalPlane
-            arView.addSubview(coachingOverlay)
-            
-            let mesh = MeshResource.generateBox(size: 0.5, cornerRadius: 0.05)
-            let material = SimpleMaterial(color: .blue, isMetallic: false)
-            let modelEntity = ModelEntity(mesh: mesh, materials: [material])
-            let anchorEntity = AnchorEntity(plane: .horizontal)
-            anchorEntity.addChild(modelEntity)
-            arView.scene.addAnchor(anchorEntity)
-            
-            modelEntity.generateCollisionShapes(recursive: true)
-            
-            arView.installGestures([.translation, .rotation, .scale], for: modelEntity)
-            
-            return arView
-            
+        func makeUIView(context: Context) -> DiceARView {
+            DiceARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
         }
         
-        func updateUIView(_ uiView: ARView, context: Context) {}
-        
+        func updateUIView(_ uiView: DiceARView, context: Context) {
+            
+            if uiView.isRolling {
+                
+            }
+        }
     }
 }
-
-#if DEBUG
-struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        DiceView()
-    }
-}
-#endif
